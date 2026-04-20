4376e2432a35670ebbecb5a67274161642e20fef
CarlosdHxH <carlosdaniel.prog@gmail.com>
2026-04-20 07:52:38 -0400

    fix

---
+++ prisma/gerarsenha.ts
@@ -0,0 +1,40 @@
+import { hash } from "bcrypt-ts";
+import { PrismaClient } from "../generated/prisma/client";
+
+const db = new PrismaClient();
+
+// ---------------------------------------------------------------------------
+// Helpers
+// ---------------------------------------------------------------------------
+
+function env(key: string, fallback?: string): string {
+  const val = process.env[key] ?? fallback;
+  if (!val) throw new Error(`Variável de ambiente obrigatória ausente: ${key}`);
+  return val;
+}
+
+// ---------------------------------------------------------------------------
+// Admin
+// ---------------------------------------------------------------------------
+
+async function seedAdmin() {
+  const email = "admin@fama.com"; //env("SEED_ADMIN_EMAIL", "admin@fama.com");
+  const password = "fama2025!"; //env("SEED_ADMIN_PASSWORD", "fama2025!");
+  const senhaHash = await hash(password, 12);
+    console.log(senhaHash)
+  await db.user.upsert({
+    where: { email },
+    update: { senha: senhaHash },
+    create: {
+      email,
+      nome: "Administrador",
+      senha: senhaHash,
+      role: "ADMIN",
+      ativo: true,
+    },
+  });
+
+  console.log(`✓ Admin: ${email}`);
+}
+
+seedAdmin();

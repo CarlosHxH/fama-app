ba6f88df19cbb8eb9288424a87ca2caebd99059b
CarlosdHxH <carlosdaniel.prog@gmail.com>
2026-04-28 21:39:27 -0400

    Teste middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -77,6 +77,7 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
     "/login",
     "/cobranca",
     "/cobranca/:path*",
